9f412e7e897d7baf8305153940d74b565a342735
Giordano Cardillo <giordano.cardillo@gmail.com>
2019-05-24 15:06:15 +0200

    Added ES6 promises call mode.

--- www/SimpleCrypto.js
+++ www/SimpleCrypto.js
@@ -5,10 +5,21 @@ var exec = require('cordova/exec');
 var SimpleCrypto = function() {};
 
 SimpleCrypto.prototype.encrypt = function(key, data, success, error) {
+
+  if(!success && !error) {
+    return new Promise((resolve, reject) => {
+      exec(resolve, reject, "SimpleCrypto", "encrypt", [key, data]);
+    })
+  }
   exec(success, error, "SimpleCrypto", "encrypt", [key, data]);
 }
 
 SimpleCrypto.prototype.decrypt = function(key, data, success, error) {
+  if(!success && !error) {
+    return new Promise((resolve, reject) => {
+      exec(resolve, reject, "SimpleCrypto", "decrypt", [key, data]);
+    })
+  }
   exec(success, error, "SimpleCrypto", "decrypt", [key, data]);
 }
 
